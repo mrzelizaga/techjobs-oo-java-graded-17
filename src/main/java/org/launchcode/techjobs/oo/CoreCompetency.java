9a2493c58d59ad716af9cecf54886e7b4129cd62
Ella <mrzelizaga@gmail.com>
2023-10-03 19:35:38 -0600

    getters and setters

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -40,5 +40,21 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
+    // Getter for the 'id' field
+    public int getId() {
+        return id;
+    }
+
+    // Getter and Setter for the 'value' field
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
